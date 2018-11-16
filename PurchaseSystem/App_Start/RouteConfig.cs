f89e7b35f32b22f799cead2fe7b8757508d89845
Vishal Jaiswal <vjaisknit143@gmail.com>
2018-11-16 07:56:00 -0800

    Project Init

---
+++ PurchaseSystem/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PurchaseSystem
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //   ProductType/Product/1/3
+
+            routes.MapMvcAttributeRoutes();
+
+           
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ PurchaseSystem/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using PurchaseSystem.DTO;
+using PurchaseSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PurchaseSystem.Controllers
+{
+    public class ProductController : Controller
+    {
+        ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Product
+
+        [HttpGet]
+        public ActionResult AddUpdateProduct()
+        {
+            var pageLoadData = new ProductMstDTO
+            {
+                ProductTypeMstList = _db.ProductTypeMsts.ToList()
+            };
+            return View(pageLoadData);
+        }
+
+        public ActionResult ProductList()
+        {
+           
+            return View(_db.ProductMsts.ToList());
+        }
+        [HttpPost]
+        public ActionResult AddUpdateProduct(ProductMstDTO product)
+        {
+            _db.ProductMsts.Add(product.productMst);
+            _db.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+
+
+
+    }
+}
