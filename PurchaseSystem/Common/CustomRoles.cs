ae4c4995b182df66c3f953bc0d03eb5fc414c68a
Vishal Jaiswal <vjaisknit>
2018-12-06 22:39:52 +0530

    Authorize Attribute with Multiple Roles | Allow multiple roles to access controller action | Part-16

--- PurchaseSystem/Common/CustomRoles.cs
+++ PurchaseSystem/Common/CustomRoles.cs
@@ -7,6 +7,10 @@ namespace PurchaseSystem.Common
 {
     public static class CustomRoles
     {
-      
+        public const string Admin = "Admin";
+        public const string GS = "General Store";
+        public const string CS = "Cloth Store";
+        public const string Adm_GS = Admin + "," + GS;
+        public const string Adm_CS = Admin + "," + CS;
     }
 }
