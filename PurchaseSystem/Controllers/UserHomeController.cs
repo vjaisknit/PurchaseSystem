34f71b150c843b039d21e1eae15514d8022d0293
Vishal Jaiswal <vjaisknit>
2018-12-25 20:52:42 +0530

    Work on Display Module User Wise | Part-19

--- PurchaseSystem/Controllers/UserHomeController.cs
+++ PurchaseSystem/Controllers/UserHomeController.cs
@@ -1,4 +1,5 @@
-﻿using PurchaseSystem.Models;
+﻿using PurchaseSystem.Common;
+using PurchaseSystem.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +8,27 @@ using System.Web.Mvc;
 
 namespace PurchaseSystem.Controllers
 {
+    [Authorize]
     public class UserHomeController : Controller
     {
         ApplicationDbContext _db = new ApplicationDbContext();
         // GET: UserHome
         public ActionResult DisplayModule()
         {
-
-            return View(_db.ModuleMsts.Where(a=>a.IsActive==1).ToList());
+            List<ModuleMst> ModuleList;
+            if(User.IsInRole("Admin"))
+            {
+                ModuleList = _db.ModuleMsts.Where(a => a.IsActive == 1).ToList();
+            }
+            else if(User.IsInRole("General Store"))
+            {
+                ModuleList = _db.ModuleMsts.Where(a => a.IsActive == 1 && a.pk_moduleid==3).ToList();
+            }
+            else
+            {
+                ModuleList = _db.ModuleMsts.Where(a => a.IsActive == 1 && a.pk_moduleid == 1).ToList();
+            }
+            return View(ModuleList);
         }
 
     }
